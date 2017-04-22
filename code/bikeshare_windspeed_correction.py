33aee02d391e64287d79f4ac9f5b353eda7fe9c5
Nick Chen <nwchen24@Nicks-MacBook-Pro.local>
2017-04-22 08:52:58 -0600

    NC add windspeed adjustment

---
+++ code/bikeshare_windspeed_correction.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+### Place to hide functions referenced in notebook ###
+
+import numpy as np
+from time import time
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import FeatureUnion
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import preprocessing
+from sklearn.ensemble import GradientBoostingRegressor
+import feature_engineering_windspeed_correction as fe
+
+
+# Define some time based variables
+def eda_transform(df):
+    df['hour'] = df.index.hour
+    df['weekday name'] = df.index.weekday_name
+    df['weekday'] = df.index.weekday
+    df['weekofyear'] = df.index.weekofyear
+    df['month'] = df.index.month
+    df['quarter'] = df.index.quarter
+    df['year'] = df.index.year
+    df['atempsq'] = df['atemp']**2
+    df['tempsq'] = df['temp']**2
+    temp_df = df.copy()
+    temp_df['temp'] = ('0' + df['month'].astype(str))
+    df['year_month']=df.year.astype(str) + temp_df.temp.str[-2:]
+    return df
+
+
+# Create boxplots for registered/casual rides by hour
+def boxplot_by_hour(df):
+    plt.figure(figsize=(15, 5))
+    ax1 = plt.subplot(1,2,1)
+    ax2 = plt.subplot(1,2,2)
+    df.boxplot(column='registered',by='hour',ax=ax1)
+    df.boxplot(column='casual',by='hour',ax=ax2)
+    ax1.set_ylabel('rides')
+    ax2.set_ylabel('rides')
+    ax1.set_ylim(-10,900)
+    ax2.set_ylim(-10,250)
+    plt.show()
+
+
+# Create boxplots for registered/casual rides throughout time
+def boxplot_by_yearmonth(df):
+    plt.figure(figsize=(15, 5))
+    ax1 = plt.subplot(1,2,1)
+    ax2 = plt.subplot(1,2,2)
+    df.boxplot(column='registered',by='year_month',ax=ax1)
+    df.boxplot(column='casual',by='year_month',ax=ax2)
+    ax1.set_xticklabels(ax1.xaxis.get_majorticklabels(),rotation=90)
+    ax2.set_xticklabels(ax2.xaxis.get_majorticklabels(),rotation=90)
+    ax1.set_ylabel('rides')
+    ax2.set_ylabel('rides')
+    ax1.set_ylim(-10,800)
+    ax2.set_ylim(-10,250)
+    plt.show()
+
+
+# Define pipeline steps
+def define_pipeline():
+    categorical = ('season', 'holiday', 'workingday', )
+    numerical = ('datetime', 'weather', 'temp', 'atemp', 'humidity', 'windspeed',) # Datetime isn't numerical, but needs to be in the numeric branch
+    pipeline = Pipeline([
+        # Process cat & num separately, then join back together
+        ('union', FeatureUnion([ 
+            ('categorical', Pipeline([
+                ('select_cat', fe.SelectCols(cols = categorical)),
+                ('onehot', OneHotEncoder()),    
+            ])),    
+            ('numerical', Pipeline([
+                ('select_num', fe.SelectCols(cols = numerical)),
+                ('date', fe.DateFormatter()),
+                ('drop_datetime', fe.SelectCols(cols = ('datetime', 'month'), invert = True)),
+                ('temp', fe.ProcessNumerical(cols_to_square = ('temp', 'atemp', 'humidity'),)),
+                ('rollingweather', fe.RollingWindow(cols = ('weather', ))),
+                ('forecast', fe.WeatherForecast()),
+                ('windspeedadjustment', fe.WindspeedAdjustment()),
+                # ('bad_weather', fe.BinarySplitter(col = 'weather', threshold = 2)),
+                # ('filter', fe.PassFilter(col='atemp', lb = 15, replacement_style = 'mean'))
+                ('scale', StandardScaler()),    
+            ])),    
+        ])),
+        ('to_dense', preprocessing.FunctionTransformer(lambda x: x.todense(), accept_sparse=True)), 
+        ('clf', GradientBoostingRegressor(n_estimators=100,random_state=2)),
+    ])
+    return pipeline
+
+def param_tuning_graphs(train_data,dev_data,train_label,pipeline,parameter,param_values):
+
+    categorical = ('season', 'holiday', 'workingday', )
+    numerical = ('datetime', 'weather', 'temp', 'atemp', 'humidity', 'windspeed',) # Datetime isn't numerical, but needs to be in the numeric branch
+    pipeline = Pipeline([
+        # Process cat & num separately, then join back together
+        ('union', FeatureUnion([ 
+            ('categorical', Pipeline([
+                ('select_cat', fe.SelectCols(cols = categorical)),
+                ('onehot', OneHotEncoder()),    
+            ])),    
+            ('numerical', Pipeline([
+                ('select_num', fe.SelectCols(cols = numerical)),
+                ('date', fe.DateFormatter()),
+                #('drop_datetime', fe.SelectCols(cols = ('datetime'), invert = True)),
+                ('temp', fe.ProcessNumerical(cols_to_square = ('temp', 'atemp', 'humidity'),)),
+                # ('bad_weather', fe.BinarySplitter(col = 'weather', threshold = 2)),
+                # ('filter', fe.PassFilter(col='atemp', lb = 15, replacement_style = 'mean'))
+                ('scale', StandardScaler()),    
+            ])),    
+        ])),
+        ('to_dense', preprocessing.FunctionTransformer(lambda x: x.todense(), accept_sparse=True)), 
+        #('clf', GradientBoostingRegressor(n_estimators=100,random_state=2)),
+    ])
+
+    # Run train and dev data through pipeline for feature engineering
+    features = [c for c in train_data.columns if c not in ['count', 'casual', 'registered', 'log_casual', 'log_registered']]
+    fe_train_data = pipeline.fit_transform(train_data[features])
+    fe_dev_data = pipeline.transform(dev_data[features])
+
+    row_format = "{:>10}" *(6)
+    rmse_list=[]
+    for i in param_values:
+        t0 = time()
+        if parameter == 'n_estimators':
+            gb = GradientBoostingRegressor(n_estimators=i,learning_rate=0.05,max_depth=10, min_samples_leaf=20,random_state=2)
+        if parameter == 'learning_rate': 
+            gb = GradientBoostingRegressor(n_estimators=115,learning_rate=i,max_depth=10, min_samples_leaf=20,random_state=2)
+        if parameter == 'max_depth': 
+            gb = GradientBoostingRegressor(n_estimators=115,learning_rate=0.05,max_depth=i, min_samples_leaf=20,random_state=2)
+        if parameter == 'min_samples_leaf': 
+            gb = GradientBoostingRegressor(n_estimators=115,learning_rate=0.05,max_depth=10, min_samples_leaf=i,random_state=2)
+        gb.fit(fe_train_data, train_data[train_label])
+        predicted_y = gb.predict(fe_dev_data)
+        rmse = get_RMSE(actual_values = dev_data[train_label], predicted_values = predicted_y)
+        rmse_list.append(round(rmse,3))
+        print row_format.format(parameter+":", i, "RMSE:", round(rmse,3),
+                                "Runtime:", round((time() - t0),3))
+    plt.plot(param_values,rmse_list)
+    plt.show()
+    return rmse_list
+
+def param_tuning_graphs_abbrev_casual():
+    #These lists are the resulting RMSEs from the parameter tuning grid searches above. We have saved permanent lists to avoid needing to run through a length grid search to produce the desired parameter tuning graphs.
+    param_tuning_rmse_list_n_estimators = [0.216,0.216,0.216,0.215,0.215,0.215,0.215,0.215,0.216,0.216,0.216,0.216,0.216,0.216,0.216]
+    param_tuning_rmse_list_learning_rate = [0.286, 0.228, 0.216, 0.214, 0.215, 0.216, 0.217, 0.219, 0.217, 0.219, 0.218, 0.217, 0.223, 0.23, 0.232]
+    param_tuning_rmse_list_max_depth = [0.299,0.252,0.231,0.22,0.214,0.213,0.212,0.213,0.214,0.216,0.216,0.217,0.217,0.217,0.216,0.218,0.218,0.219,0.217,0.217]
+    param_tuning_rmse_list_min_samples_leaf = [0.228, 0.224, 0.219, 0.215, 0.216, 0.214, 0.214, 0.214]
+
+    plt.figure(figsize=(15, 8))
+    plt.suptitle('Parameter Tuning Gradient Boost for Casual Rides', fontsize=14, fontweight='bold')
+
+    param_values=[70,75,80,85,90,95,100,105,110,115,120,125,130,135,140]
+    plt.subplot(221)
+    plt.plot(param_values, param_tuning_rmse_list_n_estimators)
+    plt.xlim(68,142)
+    plt.ylim(.2144,.2165)
+    plt.xlabel('n_estimators')
+    plt.ylabel('RMSE')
+
+    param_values=[.01,.02,.03,.04,.045,.05,.055,.06,.07,.08,.09,.1,.2,.3,.4]
+    plt.subplot(222)
+    plt.plot(param_values, param_tuning_rmse_list_learning_rate)
+    plt.xlim(0,.41)
+    plt.ylim(.21,.29)
+    plt.xlabel('learning_rate')
+    plt.ylabel('RMSE')
+
+    param_values=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    plt.subplot(223)
+    plt.plot(param_values, param_tuning_rmse_list_max_depth)
+    plt.xlim(0,21)
+    plt.ylim(.205,.305)
+    plt.xlabel('max_depth')
+    plt.ylabel('RMSE')
+
+    param_values=[1,5,10,15,20,25,30,35]
+    plt.subplot(224)
+    plt.plot(param_values, param_tuning_rmse_list_min_samples_leaf)
+    plt.xlim(0,36)
+    plt.ylim(.213,.23)
+    plt.xlabel('min_samples_leaf')
+    plt.ylabel('RMSE')
+
+    plt.show() 
+
+    
+def train_dev_model_search(pipeline, train_data, dev_data, train_label, parameters, RMSE_scorer):
+
+    print("Performing grid search...")
+    t0 = time()
+    gs = GridSearchCV(pipeline, parameters, n_jobs=1, verbose=1, scoring=RMSE_scorer)
+    features = [c for c in train_data.columns if c not in ['count', 'casual', 'registered', 'log_casual', 'log_registered']]
+    gs.fit(train_data[features], train_data[train_label])
+    print("Best parameters set:")
+    best_param = gs.best_estimator_.get_params()
+    for param_name in sorted(parameters.keys()):
+        print("\t%s: %r" % (param_name, best_param[param_name]))
+    predicted_y = gs.predict(dev_data[features])
+    print "GridSearch RMSE " + str(gs.best_score_)
+    rmse = get_RMSE(actual_values = dev_data[train_label], predicted_values = predicted_y)
+    print "RMSE: ", str(rmse)
+    print("Done in %0.3fs" % (time() - t0))
+    print ""
+
+
+def get_RMSE(actual_values, predicted_values):
+    n = len(actual_values)
+    RMSE = np.sqrt(np.sum(((np.log(predicted_values + 1) - np.log(actual_values + 1)) ** 2) / n))
+    return RMSE
---
+++ code/feature_engineering_windspeed_correction.py
@@ -0,0 +1,344 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+import numpy as np
+
+class PipelineEstimator(BaseEstimator, TransformerMixin):
+    """Define the necessary methods"""
+
+    def __init__(self):
+        pass
+
+
+    def fit(self, X, y = None):
+        return self
+
+
+    def transform(self, X, y = None):
+        return X
+
+
+
+class Duplicate(PipelineEstimator):
+    def __init__(self, cols, suffix = "_2"):
+        self.cols = cols
+        self.suffix = suffix
+        
+    def transform(self, X, y = None):
+        for col in self.cols:
+            X[col + self.suffix] = X[col]
+        return X
+
+class Square(PipelineEstimator):
+    def transform(self, X):
+        return np.square(X)
+    
+
+
+class ProcessNumerical(PipelineEstimator):
+
+    def __init__(self, cols_to_square = (), cols_to_log = (), cols_to_sqrt = ()):
+        self.cols_to_square = cols_to_square
+        self.cols_to_log = cols_to_log
+        self.cols_to_sqrt = cols_to_sqrt
+
+
+    def square(self, X):
+        for col in self.cols_to_square:
+            X[col + '_sq'] = np.square(X[col])
+        return X
+
+
+    def log(self, X):
+        for col in self.cols_to_log:
+            X[col + '_log'] = np.log(X[col])
+        return X
+
+
+    def sqrt(self, X):
+        for col in self.cols_to_sqrt:
+            X[col + '_sqrt'] = np.sqrt(X[col])
+        return X
+
+    def transform(self, X, y = None):
+        """Square or sqrt given numerical columns"""
+        X = self.square(X)
+        X = self.log(X)
+        X = self.sqrt(X)
+
+        return X
+
+
+class RollingWindow(PipelineEstimator):
+    def __init__(self, cols = (), window = 6, style = 'mean', shift = True):
+        self.cols = cols
+        self.window = window
+        self.style = style
+        self.shift = shift
+        self.shift_val = 1 if window > 0 else -1
+        
+    def transform(self, X, y = None):
+        if self.window == 0:
+            return X
+        for col in self.cols:
+            if self.shift:
+                rolling = X[col].shift(self.shift_val)
+            else:
+                rolling = X[col]
+
+            rolling = rolling.rolling(self.window)
+
+            if self.style == 'mean':
+                rolling = rolling.mean()
+            elif self.style == 'sum':
+                rolling = rolling.sum()
+
+            X["{}_roll_{}".format(col, self.window)] = rolling.fillna(1)
+        return X
+
+
+class DailyGroup(PipelineEstimator):
+    def __init__(self, func = np.mean, cols = None, rsuffix = '_dailyavg'):
+        self.func = func
+        self.cols = cols
+        self.rsuffix = rsuffix
+
+
+    def group(self, X):
+        return X.groupby('date').apply(self.func)
+
+
+    def transform(self, X, y = None):
+        if self.cols is None:
+            return X
+
+        X['date'] = X.index.date
+        grouped = self.group(X[['date'] + self.cols])
+        X = X.join(grouped, on = 'date', how = 'left', rsuffix = self.rsuffix)
+        try: 
+            X = X.drop('date', axis = 1)
+            X = X.drop('date' + self.rsuffix, axis = 1)
+        finally:
+            return X
+
+
+class WeatherForecast(PipelineEstimator):
+    def __init__(self, use = True):
+        self.use = use
+        
+        
+    def transform(self, X, y = None):
+        if not self.use:
+            return X
+
+        # X = X.copy()
+        X['weather_was_better'] = X['weather'].shift() - X['weather']
+        X['weather_getting_better'] = X['weather'].shift(-1) - X['weather']
+        X[['weather_was_better', 'weather_getting_better']] = X[['weather_was_better', 'weather_getting_better']].fillna(0)
+        return X
+
+class WindspeedAdjustment(PipelineEstimator):
+    def __init__(self, use = True):
+        self.use = use
+        
+    def transform(self, X, y = None):
+        if not self.use:
+            return X
+        
+        X['windspeed'] = X['windspeed'].where(X['windspeed'] > 1, np.nan)
+        X['windspeed'] = X['windspeed'].ffill().bfill()
+        return X
+        
+        
+        
+class DateFormatter(PipelineEstimator):
+    """Parse datetime into its component parts"""
+
+    def __init__(self):
+        self.earliest_date = None
+
+
+    def fit(self, X, y = None):
+        self.earliest_date = np.min(X.index.date)
+        return self
+        
+
+
+    def transform(self, X, y = None):
+        """Split the datetime into its component parts."""
+
+        X['hour'] = X.index.hour
+        #X['weekday name'] = X.index.weekday_name
+        X['weekday'] = X.index.weekday
+        X['weekofyear'] = X.index.weekofyear
+        X['month'] = X.index.month
+        X['quarter'] = X.index.quarter
+        X['year'] = X.index.year
+        X['dom'] = X.index.day
+        X['weekend'] = np.where(X['weekday']>5, 1, 0)
+
+        # X['days_since_start'] = X.index.date - self.earliest_date
+        # X['days_since_start'] = X['days_since_start'].apply(lambda x: x.days)
+
+        return X
+
+class SelectCols(PipelineEstimator):
+    """Return only specified columnss"""
+
+    def __init__(self, cols = (), invert = False):
+        self.cols = cols
+        self.invert = invert
+
+
+    def transform(self, X, y = None):
+        mask = np.array([True if col in self.cols else False for col in X.columns])
+        if self.invert:
+            mask = np.invert(mask)
+        return X.loc[:, mask]
+
+
+class BinSeparator(PipelineEstimator):
+    """Separate specified column into bins and return a new column whose values are these bins"""
+    
+    def __init__(self, col = (), bin_edges = (), bin_labels = ()):
+        self.col = col
+        self.bin_edges = bin_edges
+        self.bin_labels = bin_labels
+        
+    def transform(self, X, y = None):
+        X[self.col] = X[self.col].astype('int64')
+        X[self.col + '_bin'] = pd.cut(X[self.col], bins = self.bin_edges, labels = self.bin_labels)
+        return X
+
+class BinarySplitter(PipelineEstimator):
+    """Binarize a feature and add that as a new feature"""
+
+    def __init__(self, col, threshold, new_name = None):
+        """Split col based on threshold"""
+        self.col = col
+        self.threshold = threshold
+        self.new_name = new_name or col + "_bin"
+
+
+    def transform(self, X, y = None):
+        X[self.new_name] = X[self.col] >= self.threshold
+        return X
+
+
+class PassFilter(PipelineEstimator):
+    """Low, hi, or bandpass filter
+
+    If you supply:
+    a lowerbound (lb): you will get a high pass filter.
+    an upperbound (ub): you will get a low pass filter.
+    both lb and ub: you will get a band pass filter.
+    
+    You may specify the values you want to replace values that do not
+    pass. If neither the replacements nor the replacement style are
+    supplied, then the low values will be replaced with the lb, and
+    high values with the ub.
+
+    If you specify the replacement style to be:
+    'med': Values will be replaced by the median of the values that do
+    not pass on that side
+    'mean': Ditto, but with the mean
+    'minmax': The minimum value out of the low values, and the max of
+    the high values
+    """
+
+    def __init__(self, col, lb = None, ub = None, new_name = None,
+                 lb_replacement = None, ub_replacement = None,
+                 replacement_style = None):
+        self.col = col
+        self.lb  = lb
+        self.ub  = ub
+        self.new_name = new_name or col + "_filt"
+        self.lb_replacement = lb_replacement
+        self.ub_replacement = ub_replacement
+        self.replacement_style = replacement_style
+
+
+    def fit(self, X, y = None):
+        """Determine replacement values if none given"""
+        if self.lb and self.lb_replacement is None:
+
+            if self.replacement_style == 'min_max':
+                self.lb_replacement = np.min(X[self.col])
+
+            elif self.replacement_style == 'med':
+                med = np.nanmedian(
+                    np.where(X[self.col] > self.lb, X[self.col], np.nan))
+                self.lb_replacement = med
+
+            elif self.replacement_style == 'mean':
+                mean = np.nanmean(
+                    np.where(X[self.col] > self.lb, X[self.col], np.nan))
+                self.lb_replacement = mean
+
+            else:
+                self.lb_replacement = self.lb
+
+
+        if self.ub and self.ub_replacement is None:
+
+            if self.replacement_style == 'min_max':
+                self.ub_replacement = np.max(X[self.col])
+
+            elif self.replacement_style == 'med':
+                med = np.nanmedian(
+                    np.where(X[self.col] > self.ub, X[self.col], np.nan))
+                self.ub_replacement = med
+
+            elif self.replacement_style == 'mean':
+                mean = np.nanmean(
+                    np.where(X[self.col] > self.ub, X[self.col], np.nan))
+                self.ub_replacement = mean
+
+            else:
+                self.ub_replacement = self.ub
+
+        return self
+        
+    def transform(self, X, y = None):
+        X[self.new_name] = X[self.col]
+
+        if self.lb:
+            X[self.new_name] = np.where(
+                X[self.new_name] < self.lb, self.lb_replacement, X[self.new_name]
+            )
+
+        if self.ub:
+            X[self.new_name] = np.where(
+                X[self.new_name] > self.ub, self.ub_replacement, X[self.new_name]
+            )
+
+        return X
+        
+        
+        
+
+class RegWrapper(PipelineEstimator):
+    def __init__(self, clf, y, func = None):
+        self.clf = clf
+        self.y = y
+        self.func = func
+
+    
+    def fit(self, X, y = None):
+        print(len(X))
+        print(len(y))
+        print(len(self.y))
+        self.clf.fit(X, self.y)
+        return self
+
+
+    def transform(self, X, y = None):
+        print(len(X))
+        print(len(y))
+        preds = self.clf.predict(X)
+        if self.func is not None:
+            return self.func(preds)
+        else:
+            return preds
